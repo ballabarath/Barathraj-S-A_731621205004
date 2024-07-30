84c8c051b65acb0377dae00f5b05f61f01a79583
ballabarath <barathraj9905@gmail.com>
2024-07-30 16:43:32 +0530

    some files added

--- Frontend Programs/Angular-Day1/watermanagement/src/app/curd-main/curd-main.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'crud-main',
-  templateUrl: './crud-main.component.html',
-  styleUrls: ['./crud-main.component.css']
-})
-export class CrudMainComponent { }
